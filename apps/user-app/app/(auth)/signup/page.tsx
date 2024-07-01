e1d31659fbc910919f7ef24ab13011759572dfe7
thejasveer13 <thejasveer13@gmail.com>
2024-07-01 06:50:46 -0500

    d

--- apps/user-app/app/(auth)/signup/page.tsx
+++ apps/user-app/app/(auth)/signup/page.tsx
@@ -51,7 +51,7 @@ export default function() {
             }
 
           }
-
+ 
     return  <section className="    ">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     <div className="text-2xl "> <Logo/></div>
