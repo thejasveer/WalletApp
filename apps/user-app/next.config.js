8dbf91504c883a764d6802b2e713c362b1ab85fc
thejasveer13 <thejasveer13@gmail.com>
2024-07-05 06:34:11 -0500

    env

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config();
+console.log('Environment Variables:', process.env.NEXT_PUBLIC_WEBSOCKET_URL);
+
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  env:{
+    NEXT_PUBLIC_JWT_SECRET:process.env.NEXT_PUBLIC_JWT_SECRET,
+    NEXT_PUBLIC_NETBANKING_URL:process.env.NEXT_PUBLIC_NETBANKING_URL,
+    NEXT_PUBLIC_SIGNUP_NETBANKING_URL:process.env.NEXT_PUBLIC_SIGNUP_NETBANKING_URL,
+    NEXT_PUBLIC_NETBANKING_SECRET:process.env.NEXT_PUBLIC_NETBANKING_SECRET,
+    NEXT_PUBLIC_WEBSOCKET_URL:process.env.NEXT_PUBLIC_WEBSOCKET_URL,
+    NEXT_PUBLIC_SERVER_WEBHOOK_URL:process.env.NEXT_PUBLIC_SERVER_WEBHOOK_URL,
+   }
 };
