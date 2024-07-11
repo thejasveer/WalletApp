883708ea59a806dfaf5b00950ac8dc6b34172ef0
thejasveer13 <thejasveer13@gmail.com>
2024-07-11 05:07:50 -0500

    pr'

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -4,15 +4,13 @@ import { authOptions } from "../../lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-import getConfig from 'next/config'
+ 
 
 
 
 export default async function () {
     const session = await getServerSession(authOptions);
-    const { publicRuntimeConfig: config } = getConfig()
-    
-    console.log('config:', JSON.stringify(config))
+     
     if(!session)
         {
          redirect('/signin')
